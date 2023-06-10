51aef901ef24e2a03992350b5adacda6ecc7db1f
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-06-10 11:27:26 -0400

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,18 @@
 import { authMiddleware } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
 
-export default authMiddleware();
+export default authMiddleware({
+  publicRoutes: ['/'],
+  afterAuth(auth, req) {
+    // handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      const signInUrl = new URL('/sign-in', req.url);
+      signInUrl.searchParams.set('redirect_url', req.url);
+      return NextResponse.redirect(signInUrl);
+    }
+  },
+});
