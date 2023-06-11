a0fbb0faa90d789aab24c0d07b2980438fa89938
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-06-10 23:15:00 -0400

    type safe translation hook?

---
+++ src/hooks/useAppTranslation.ts
@@ -0,0 +1,26 @@
+import enUSLocale from '@/i18n/locales/en-US';
+import { useTranslation } from 'react-i18next';
+
+type DotPrefix<T extends string> = T extends '' ? '' : `.${T}`;
+
+type DotNestedKeys<T> = (
+  T extends object
+    ? {
+        [K in Exclude<keyof T, symbol>]: `${K}${DotPrefix<
+          DotNestedKeys<T[K]>
+        >}`;
+      }[Exclude<keyof T, symbol>]
+    : ''
+) extends infer D
+  ? Extract<D, string>
+  : never;
+
+type LocaleKeys = DotNestedKeys<typeof enUSLocale>;
+
+const useAppTranslation = (key: LocaleKeys) => {
+  const { t } = useTranslation();
+
+  return t(key);
+};
+
+export default useAppTranslation;
