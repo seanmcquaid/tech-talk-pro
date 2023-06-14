66d5701bad281ed3df0d6e540639f0801a795df2
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-06-14 18:13:47 -0400

    some file move around

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ import '@/i18n/client';
 import 'antd/dist/reset.css';
 import { PropsWithChildren } from 'react';
 import GlobalStyle from '@/styles/GlobalStyle';
-import AppLayout from '@/components/AppLayout';
 import '@/env.client';
 import '@/env.server';
 
@@ -25,9 +24,7 @@ const RootLayout = ({ children }: PropsWithChildren) => {
           <Analytics />
           <body>
             <ReduxProvider>
-              <InitializeApp>
-                <AppLayout>{children}</AppLayout>
-              </InitializeApp>
+              <InitializeApp>{children}</InitializeApp>
             </ReduxProvider>
           </body>
         </html>
