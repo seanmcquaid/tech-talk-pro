f2eeda5536ee76255b3341765415e093194fdc06
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-06-21 22:24:57 -0500

    Dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,30 @@
+'use client';
+import useAppTranslation from '@/hooks/useAppTranslation';
+import { Card, Typography } from 'antd';
+import { useRouter } from 'next/navigation';
+import styled from 'styled-components';
+
 const Dashboard = () => {
-  return <div>{'Dashboard'}</div>;
+  const router = useRouter();
+  const { t } = useAppTranslation();
+
+  return (
+    <>
+      <StyledCard
+        title={t('Dashboard.talksCardTitle')}
+        size="default"
+        onClick={() => router.push('/dashboard/talks')}
+      >
+        <Typography.Paragraph>
+          {t('Dashboard.talksCardInfo')}
+        </Typography.Paragraph>
+      </StyledCard>
+    </>
+  );
 };
+
+const StyledCard = styled(Card)`
+  cursor: pointer;
+`;
 
 export default Dashboard;
