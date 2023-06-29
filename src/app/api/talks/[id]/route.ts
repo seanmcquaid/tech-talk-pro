8e8f4e8f60f9377a728ead387028d8ac35bdeab4
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-06-28 22:10:34 -0400

    talk api route

--- src/app/api/talks/[id]/route.ts
+++ src/app/api/talks/[id]/route.ts
@@ -1,5 +1,29 @@
-import { NextResponse } from 'next/server';
+import db from '@/utils/db';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET() {
-  return NextResponse.json([]);
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get('id');
+
+  if (!id) {
+    return NextResponse.next({
+      status: 400,
+      statusText: 'No ID was provided',
+    });
+  }
+
+  const talk = await db.talk.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!talk) {
+    return NextResponse.next({
+      status: 404,
+      statusText: 'Talk not found',
+    });
+  }
+
+  return NextResponse.json(talk);
 }
