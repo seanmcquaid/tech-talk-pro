439bd0b596b91d6425bf9cb4bfc9e1a8e25d4b7b
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-06-29 22:55:08 -0400

    create slide

--- src/app/api/slides/[talkId]/route.ts
+++ src/app/api/slides/[talkId]/route.ts
@@ -1,3 +1,4 @@
+import { createSlideBodySchema } from '@/types/requests/CreateSlideBody';
 import db from '@/utils/db';
 import { auth } from '@clerk/nextjs';
 import { NextRequest, NextResponse } from 'next/server';
@@ -25,7 +26,29 @@ export async function GET(request: NextRequest) {
 }
 
 export async function POST(request: NextRequest) {
+  const { userId } = auth();
+  const { searchParams } = new URL(request.url);
+  const talkId = searchParams.get('talkId');
   const res = await request.json();
 
-  return NextResponse.json(res);
+  try {
+    const body = createSlideBodySchema.parse(res);
+    const { title, sortOrder, bulletPoints, notes } = body;
+    const slide = await db.slide.create({
+      data: {
+        title,
+        talkId: talkId!,
+        userId: userId!,
+        sortOrder,
+        bulletPoints,
+        notes,
+      },
+    });
+    return NextResponse.json(slide);
+  } catch (err) {
+    return NextResponse.next({
+      status: 400,
+      statusText: "The slide couldn't be created",
+    });
+  }
 }
