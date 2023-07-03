686175ada2657767865ece665525b52b4e5c1df8
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-07-03 13:43:56 -0400

    finished apis, going to write tests and refactor

--- src/app/api/talks/[id]/route.ts
+++ src/app/api/talks/[id]/route.ts
@@ -1,3 +1,4 @@
+import { createTalkBodySchema } from '@/types/requests/CreateTalkBody';
 import db from '@/utils/db';
 import { auth } from '@clerk/nextjs';
 import { NextRequest, NextResponse } from 'next/server';
@@ -35,4 +36,53 @@ export async function GET(request: NextRequest) {
   }
 
   return NextResponse.json(talk);
+}
+
+export function DELETE(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get('id');
+
+  try {
+    db.talk.delete({
+      where: {
+        id: id!,
+      },
+    });
+    return NextResponse.next({
+      status: 204,
+    });
+  } catch (err) {
+    return NextResponse.next({
+      status: 400,
+      statusText: "The talk couldn't be deleted",
+    });
+  }
+}
+
+export async function PUT(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get('id');
+  const res = await request.json();
+
+  try {
+    const { title, talkLength, topic, abstract } =
+      createTalkBodySchema.parse(res);
+    const talk = await db.talk.update({
+      where: {
+        id: id!,
+      },
+      data: {
+        title,
+        talkLength,
+        topic,
+        abstract,
+      },
+    });
+    return NextResponse.json(talk);
+  } catch (err) {
+    return NextResponse.next({
+      status: 400,
+      statusText: "The talk couldn't be updated",
+    });
+  }
 }
