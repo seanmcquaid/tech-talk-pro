82ac5767d31bb7f022704c15e5045a00ce88edc2
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-07-03 22:33:36 -0400

    antd upgrade

---
+++ src/app/api/slides/[talkId]/[slideId]/__tests__/route.test.ts
@@ -0,0 +1,12 @@
+/* eslint-disable vitest/expect-expect */
+/* eslint-disable @typescript-eslint/no-empty-function */
+describe('/slides/[talkId]/[slideId]', () => {
+  describe('DELETE', () => {
+    it('returns a 204 on successful request', async () => {});
+    it('returns a 500 if the slide could not be deleted', async () => {});
+  });
+  describe('PUT', () => {
+    it('returns a 200 on successful request', async () => {});
+    it('returns a 500 if the slide could not be updated', async () => {});
+  });
+});
---
+++ src/app/api/slides/__tests__/route.test.ts
@@ -0,0 +1,12 @@
+/* eslint-disable vitest/expect-expect */
+/* eslint-disable @typescript-eslint/no-empty-function */
+describe('/slides/[talkId]', () => {
+  describe('GET', () => {
+    it('returns a 200 on successful request', async () => {});
+    it('returns a 500 if the slides could not be retrieved', async () => {});
+  });
+  describe('POST', () => {
+    it('returns a 200 on successful request', async () => {});
+    it('returns a 500 if the slide could not be created', async () => {});
+  });
+});
