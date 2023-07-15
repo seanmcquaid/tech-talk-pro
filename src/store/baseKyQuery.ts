e80aaf8360249c780b6eb6924b954ebff003f120
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-07-14 22:17:39 -0400

    ky updates

---
+++ src/store/baseKyQuery.ts
@@ -0,0 +1,30 @@
+import type { BaseQueryFn } from '@reduxjs/toolkit/dist/query';
+import type { HTTPError, Options } from 'ky';
+import ky from 'ky';
+
+interface KyBaseQuery extends Options {
+  url: string;
+}
+
+const kyBaseQuery =
+  (
+    { baseUrl }: { baseUrl: string } = { baseUrl: '' },
+  ): BaseQueryFn<KyBaseQuery, unknown, unknown> =>
+  async ({ url, ...rest }) => {
+    try {
+      const result = await ky(baseUrl + url, {
+        ...rest,
+      });
+      return { data: await result.json() };
+    } catch (err) {
+      const httpError = err as HTTPError;
+      return {
+        error: {
+          status: httpError.response.status,
+          data: httpError.responseData,
+        },
+      };
+    }
+  };
+
+export default kyBaseQuery;
