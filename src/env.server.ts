3c44dbb54d43d6bb244c14f5bf9f5b97b96e0f14
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-07-16 12:58:33 -0400

    added ai sdk and key to env

--- src/env.server.ts
+++ src/env.server.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 const serverEnvSchema = z.object({
   CLERK_SECRET_KEY: z.string(),
   DATABASE_URL: z.string(),
+  OPEN_AI_API_KEY: z.string(),
 });
 
 const serverEnv = serverEnvSchema.parse(process.env);
