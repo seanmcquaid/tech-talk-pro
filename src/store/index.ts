597af8ae3f7f8014e7373d7f5b5a86750bb86eac
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-07-21 15:44:02 -0400

    remove comments

--- src/store/index.ts
+++ src/store/index.ts
@@ -25,10 +25,9 @@ const store = configureStore({
 
 export const persistor = persistStore(store);
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
-export const useAppDispatch: () => AppDispatch = useDispatch; // Export a hook that can be reused to resolve types
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
 
 export default store;
