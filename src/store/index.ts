f159c9dc3dcb828006d4e6847e9092d5510b592a
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-08-13 21:10:10 -0400

    store addition

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { useDispatch } from 'react-redux';
+import type { TypedUseSelectorHook } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   persistStore,
   FLUSH,
@@ -29,5 +30,6 @@ export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export default store;
