6d71ff88fc0454e0af5d0617597de95ff47b4631
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-08-18 22:36:06 -0400

    setup tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,4 @@
-import type { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers';
-import matchers from '@testing-library/jest-dom/matchers';
-import { expect } from 'vitest';
+import '@testing-library/jest-dom/vitest';
 import type { PrismaClient } from '@prisma/client';
 import type { DeepMockProxy } from 'vitest-mock-extended';
 import { mockDeep } from 'vitest-mock-extended';
@@ -20,11 +18,3 @@ vi.mock('react-i18next', () => ({
 }));
 
 export const mockDb = db as unknown as DeepMockProxy<PrismaClient>;
-
-declare module 'vitest' {
-  interface Assertion<T>
-    extends jest.Matchers<void, T>,
-      TestingLibraryMatchers<T, void> {}
-}
-
-expect.extend(matchers);
